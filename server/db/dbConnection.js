0c82009383343cd09c29692ba84bd4e09bccf52c
Kevin Otuteye <kevinotuteye@gmail.com>
2024-03-25 14:03:20 +0000

    push

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -1,5 +1,12 @@
 const mysql = require('mysql');
 
+console.log({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
 // MySQL database connection configuration
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
