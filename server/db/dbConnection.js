176c239237fbebdfacc8249ad765a89c095853ff
Kevin Otuteye <kevinotuteye@gmail.com>
2024-03-25 14:01:02 +0000

    add proc to db connection

--- server/db/dbConnection.js
+++ server/db/dbConnection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 // MySQL database connection configuration
 const connection = mysql.createConnection({
-    host: DB_HOST,
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 // Connect to the database
